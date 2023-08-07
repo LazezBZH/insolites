9077fd8a408620da1663da2500b2e9527dfb30ac
LazezBZH <lazez.bzh@caramail.fr>
2023-08-07 23:22:50 +0200

    favicon

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -9,11 +9,11 @@ function createNav() {
       objects = data.objects;
       for (let i = 0; i < 6; i++) {
         let obj = objects[i];
-        nav1.innerHTML += ` <li ><a href="card.html?id=${obj.id}" class="button button-nav">${obj.name}<img class="mini" src="assets/images/${obj.name}.jpg"/></a></li> `;
+        nav1.innerHTML += ` <li ><a href="card.html?id=${obj.id}" class="button button-nav">${obj.name}<img class="mini" alt=${obj.name} src="assets/images/${obj.name}.jpg"/></a></li> `;
       }
       for (let i = 6; i < objects.length; i++) {
         let obj = objects[i];
-        nav2.innerHTML += `<li><a href="card.html?id=${obj.id}" class="button">${obj.name}<img class="mini" src="assets/images/${obj.name}.jpg"/></a></li>`;
+        nav2.innerHTML += `<li><a href="card.html?id=${obj.id}" class="button">${obj.name}<img class="mini" alt=${obj.name} src="assets/images/${obj.name}.jpg"/></a></li>`;
       }
     });
 }
